c18796783f6fae7f3167ca264ce01a7ac2dbb4c0
Brendon Willian <bmota@its.jnj.com>
2018-11-07 15:51:53 -0200

    Removed border

--- src/components/Sticker.js
+++ src/components/Sticker.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Image } from 'react-native';
+import { View, Image } from 'react-native';
 
 export default class Sticker extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Image style={{ width: this.props.width - 2, height: this.props.height }}
+      <View>
+        <Image style={{ width: this.props.width, height: this.props.height }}
           source={{ uri: `data:image/png;base64,${this.props.sticker}` }} />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    borderColor: 'white',
-    borderWidth: 1,
-  }
-});
