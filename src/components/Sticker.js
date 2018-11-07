bfdca184e9cefce4202f6ec0e169cb102482e026
Brendon Willian <bmota@its.jnj.com>
2018-11-07 11:28:16 -0200

    Replaced remove button to icon

--- src/components/Sticker.js
+++ src/components/Sticker.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Image, Button } from 'react-native';
+import { StyleSheet, View, Image, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class Sticker extends Component {
   render() {
     return (
       <View>
+        <View style={styles.remove}>
+          <TouchableOpacity onPress={this.props.onRemove}>
+            <Icon name="remove" size={20} color="#900" />
+          </TouchableOpacity>
+        </View>
         <Image style={styles.image} source={{ uri: `data:image/png;base64,${this.props.sticker}` }} />
-        <Button title='Remove' onPress={this.props.onRemove}></Button>
       </View>
     );
   }
@@ -17,4 +22,9 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
   },
+  remove: {
+    alignItems: 'flex-end',
+    marginBottom: -15,
+    zIndex: 1,
+  }
 });
