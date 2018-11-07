7140148877938dfaa4f6009eb3a6667f223bcf98
Brendon Willian <bmota@its.jnj.com>
2018-11-07 14:27:26 -0200

    Added float button

--- src/views/StickerList.js
+++ src/views/StickerList.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Button, NativeModules } from 'react-native';
+import { StyleSheet, View, NativeModules } from 'react-native';
 import ImagePicker from 'react-native-image-crop-picker';
 import sticker from 'StickersWpp/src/services/sticker';
 import Sticker from 'StickersWpp/src/components/Sticker';
+import ActionButton from 'react-native-action-button';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 export default class StickerList extends Component {
   constructor(props) {
@@ -42,10 +45,18 @@ export default class StickerList extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Button title='Select new image' onPress={this.selectImage.bind(this)} />
-        <Button title='Export to WhatsApp' onPress={this.exportToWpp.bind(this)} />
-        <Button title='Remove all stickers' onPress={this.removeAllStickers.bind(this)} />
         {this.renderStickerColumns()}
+        <ActionButton buttonColor="rgba(231,76,60,1)">
+          <ActionButton.Item buttonColor='#9b59b6' title="Select Image" onPress={this.selectImage.bind(this)}>
+            <Ionicons name="md-image" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor='#1abc9c' title="Export to WhatsApp" onPress={this.exportToWpp.bind(this)}>
+            <FontAwesome name="whatsapp" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor='rgba(231,76,60,1)' title="Remove All" onPress={this.removeAllStickers.bind(this)}>
+            <Ionicons name="md-trash" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+        </ActionButton>
       </View>
     );
   }
@@ -53,6 +64,12 @@ export default class StickerList extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     alignItems: 'center',
+  },
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
   },
 });
